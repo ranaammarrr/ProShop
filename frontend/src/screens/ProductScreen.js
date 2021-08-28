df2e9b0628f0ed3f36712c4db40461a03eb8fb7b
Ammar <ranaammar575@gmail.com>
2021-08-29 02:08:26 +0500

    mongodb_section4

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import {
   Row,
@@ -9,11 +9,27 @@ import {
   ListGroupItem,
   Button,
 } from 'react-bootstrap'
-import products from '../products'
 import Rating from '../components/Rating'
+import axios from 'axios' 
+
+
+
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  },[match])
+
+
   return (
     <>
       <Link className='btn btn-outline-dark  my-3' to='/'>
